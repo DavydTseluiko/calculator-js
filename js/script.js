a87513709e0bf847355b35d5f7e8cc8e96f39c1b
Davyd Tseluiko <tseluiko.davyd@gmail.com>
2024-07-03 16:33:07 -0500

    Create function operate
    Function take three arguments firstOperand, operator and secondOperand. And depends on the operator calls function like add() or multiply(). And returns the result

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
-let firstOperand = [];
-let operator;
-let secondOperand = [];
+// let firstOperand = [];
+let operator = askForOperator();
+// let secondOperand = [];
 
 let firstNumber = askForNumber("first");
 let secondNumber = askForNumber("second");
@@ -14,7 +14,23 @@ function askForNumber(text) {
   return number;
 }
 
+function askForOperator() {
+  let operator;
+
+  do {
+    operator = prompt("Enter an operator (+, -, *, /)");
+  } while (
+    operator !== "+" &&
+    operator !== "-" &&
+    operator !== "*" &&
+    operator !== "/"
+  );
+
+  return operator;
+}
+
 console.log("firsNumber: " + firstNumber);
+console.log("operator: ", operator);
 console.log("secondNumber: " + secondNumber);
 
 function add(a, b) {
@@ -36,7 +52,22 @@ function divide(a, b) {
   return a / b;
 }
 
+function operate(firstOperand, operator, secondOperand) {
+  switch (operator) {
+    case "+":
+      return add(firstOperand, secondOperand);
+    case "-":
+      return subtract(firstOperand, secondOperand);
+    case "*":
+      return multiply(firstOperand, secondOperand);
+    case "/":
+      return divide(firstOperand, secondOperand);
+  }
+}
+
 console.log("add: " + add(firstNumber, secondNumber));
 console.log("subtract: " + subtract(firstNumber, secondNumber));
 console.log("multiply: " + multiply(firstNumber, secondNumber));
 console.log("divide: " + divide(firstNumber, secondNumber));
+console.log("");
+console.log(operate(firstNumber, operator, secondNumber));
