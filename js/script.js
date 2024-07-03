de59b05a372814a8f5cf0c83ddeaa0f4f49e4fa3
Davyd Tseluiko <tseluiko.davyd@gmail.com>
2024-07-03 16:07:27 -0500

    Create functions for calculations
    Functions such as add, divide and so on

---
+++ js/script.js
@@ -0,0 +1,38 @@
+let firstNumber = askForNumber("first");
+let secondNumber = askForNumber("second");
+
+function askForNumber(text) {
+  let number;
+  do {
+    number = Number(prompt(`Enter ${text} number`));
+  } while (isNaN(number) || number == undefined);
+
+  return number;
+}
+
+console.log("firsNumber: " + firstNumber);
+console.log("secondNumber: " + secondNumber);
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  if (a / b === Infinity) {
+    return "You can not divide by 0";
+  }
+  return a / b;
+}
+
+console.log("add: " + add(firstNumber, secondNumber));
+console.log("subtract: " + subtract(firstNumber, secondNumber));
+console.log("multiply: " + multiply(firstNumber, secondNumber));
+console.log("divide: " + divide(firstNumber, secondNumber));
